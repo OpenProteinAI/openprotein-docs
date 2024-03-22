909d4befbd2b83edb33d37b048b0f91a077fab29
Mark Gee <jw.ziggee@gmail.com>
2024-03-22 22:41:18 +0900

    add fold

---
+++ src/js/swaggerFold.js
@@ -0,0 +1,45 @@
+import addSwaggerEndpointsToTOC from "./addSwaggerEndpointsToTOC.js";
+import getSwaggerJson from "./getSwaggerJson.js";
+import getBackendUrl from "./getBackendUrl.js";
+
+// get swagerSpecs manipulated
+// const swagerSpecs = await getSwaggerJson("embeddings");
+
+SwaggerUIBundle({
+  // spec: swagerSpecs,
+  url: "../openapi/fold.yml",
+  dom_id: "#swagger-ui",
+  config: {
+    deepLinking: true,
+    tagsSorter: "alpha",
+    docExpansion: "list",
+  },
+  requestInterceptor: (request) => {
+    if (request.url.includes("openapi") && request.url.endsWith(".yml")) {
+      return request;
+    }
+    var requestPath = request.url;
+    if (requestPath.startsWith("http")) {
+      requestPath = requestPath.split("/").slice(3).join("/");
+    }
+    if (requestPath.startsWith("/")) {
+      requestPath = requestPath.slice(1);
+    }
+    const backendUrl = getBackendUrl();
+    request.url = backendUrl + requestPath;
+
+    return request;
+  },
+  responseInterceptor: async (res) => {
+    if (res.data.type === "application/json5") {
+      const text = await res.data.text();
+
+      res.data = text;
+      res.text = text;
+    }
+
+    return res;
+  },
+});
+
+addSwaggerEndpointsToTOC(0);
